33b4d8219e4a698e725412e444ead0ba6f0524d0
Daniil Burnashov <danil-burnashov@mail.ru>
2022-11-10 10:14:39 +0300

    no message

--- VKApp/VKApp/TabBar/First/SortedFriendsPhotosViewController.swift
+++ VKApp/VKApp/TabBar/First/SortedFriendsPhotosViewController.swift
@@ -8,7 +8,9 @@ final class SortedFriendsPhotosViewController: UIViewController {
 
     @IBOutlet var friendsPhotoImageview: UIImageView!
 
-    var swipeAnimation = UIViewPropertyAnimator()
+    // MARK: - Private properties
+
+    private let swipeAnimation = UIViewPropertyAnimator()
 
     private var usersPhotosNames: [String] = []
     private var index = 0
@@ -21,9 +23,13 @@ final class SortedFriendsPhotosViewController: UIViewController {
         createGestures()
     }
 
+    // MARK: - Public methods
+
     func getUserPhotos(photos: [String]) {
         usersPhotosNames = photos
     }
+
+    // MARK: - Private methods
 
     private func setImage() {
         guard let photo = usersPhotosNames.first, let image = UIImage(named: photo) else { return }
@@ -32,40 +38,90 @@ final class SortedFriendsPhotosViewController: UIViewController {
     }
 
     private func createGestures() {
-        let leftSwipeGesture = UIPanGestureRecognizer(target: self, action: #selector(swipeLeft(gesture:)))
-        friendsPhotoImageview.addGestureRecognizer(leftSwipeGesture)
+        let swipeGesture = UIPanGestureRecognizer(target: self, action: #selector(swipeAction(gesture:)))
+        friendsPhotoImageview.addGestureRecognizer(swipeGesture)
+
+        let dismissGesture = UIPanGestureRecognizer(target: self, action: #selector(dismissAction))
+        view.addGestureRecognizer(dismissGesture)
     }
 
-    @objc func swipeLeft(gesture: UIPanGestureRecognizer) {
-        guard index < 2 else { return }
-        let transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+    private func swipeRight(gesture: UIPanGestureRecognizer) {
+        guard index != 2 else { return }
         switch gesture.state {
         case .began:
-            swipeAnimation.startAnimation()
-            swipeAnimation = UIViewPropertyAnimator(
-                duration: 0.5,
-                dampingRatio: 0.5,
-                animations: {
-                    self.friendsPhotoImageview.transform = CGAffineTransform(
-                        translationX: -300,
-                        y: 0
-                    )
-                }
-            )
-            swipeAnimation.pauseAnimation()
+            UIView.animate(withDuration: 0.5, delay: 0) {
+                self.friendsPhotoImageview.transform = CGAffineTransform(
+                    translationX: -300,
+                    y: 0
+                )
+            }
         case .changed:
             let translation = gesture.translation(in: view)
-            swipeAnimation.fractionComplete = translation.x / -200
-        case .ended:
-            swipeAnimation.startAnimation()
-            swipeAnimation.stopAnimation(true)
-            swipeAnimation.addAnimations {
-                self.friendsPhotoImageview.transform = .init(translationX: 300, y: 0)
+            swipeAnimation.fractionComplete = translation.x / -300
+            UIView.animate(withDuration: 0.3, delay: 0) {
+                self.friendsPhotoImageview.alpha = 0
             }
-            swipeAnimation
-            swipeAnimation.startAnimation()
+        case .ended:
+            index += 1
+            UIView.animateKeyframes(withDuration: 2, delay: 0) {
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 1) {
+                    self.friendsPhotoImageview.transform = .init(translationX: 300, y: 0)
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0.12, relativeDuration: 0.3) {
+                    self.friendsPhotoImageview.alpha = 1
+                    self.friendsPhotoImageview.transform = .init(translationX: 0, y: 0)
+                }
+            }
         default:
             return
         }
+    }
+
+    private func swipeLeft(gesture: UIPanGestureRecognizer) {
+        guard index != 0 else { return }
+        switch gesture.state {
+        case .began:
+            UIView.animate(withDuration: 0.5, delay: 0) {
+                self.friendsPhotoImageview.transform = CGAffineTransform(
+                    translationX: 300,
+                    y: 0
+                )
+            }
+        case .changed:
+            let translation = gesture.translation(in: view)
+            swipeAnimation.fractionComplete = translation.x / 300
+            UIView.animate(withDuration: 0.3, delay: 0) {
+                self.friendsPhotoImageview.alpha = 0
+            }
+        case .ended:
+            index -= 1
+            UIView.animateKeyframes(withDuration: 2, delay: 0) {
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 1) {
+                    self.friendsPhotoImageview.transform = .init(translationX: -300, y: 0)
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0.12, relativeDuration: 0.3) {
+                    self.friendsPhotoImageview.alpha = 1
+                    self.friendsPhotoImageview.transform = .init(translationX: 0, y: 0)
+                }
+            }
+        default:
+            return
+        }
+    }
+
+    @objc func swipeAction(gesture: UIPanGestureRecognizer) {
+        if gesture.translation(in: view).x < 0 {
+            swipeRight(gesture: gesture)
+        } else {
+            swipeLeft(gesture: gesture)
+        }
+        friendsPhotoImageview.image = UIImage(named: usersPhotosNames[index])
+    }
+
+    @objc private func dismissAction(gesture: UIPanGestureRecognizer) {
+        let velocity = gesture.velocity(in: view)
+        let swipe = velocity.y > 500
+        guard swipe else { return }
+        dismiss(animated: true)
     }
 }
